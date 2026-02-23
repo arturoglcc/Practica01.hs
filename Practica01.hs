76f2ada988cd406085235265cba60a7870c826b4
Arturo Guerrero <arturo_guerrero@ciencias.unam.mx>
2026-02-22 19:10:59 -0600

    Ejercicio1

--- Practica01.hs
+++ Practica01.hs
@@ -1,2 +1,9 @@
 -- Práctica 1. Arturo Guerrero López, Edgar
 
+data List a = Void | Cons a ( List a )
+
+potencia :: Double -> Int -> Double
+potencia _ 0 = 1.0
+potencia base exp =
+    if exp < 0 then 1.0 / potencia base (-exp)
+    else  base * potencia  base (exp - 1)
